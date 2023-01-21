af054b3668f57c411fab16b91aa654a0f2382db3
Shobhan-Pakhira <87083191+Shobhan-Pakhira@users.noreply.github.com>
2023-01-21 22:46:11 +0530

    Finishing up the pages

--- scrollreact/src/components/LandingPage.js
+++ scrollreact/src/components/LandingPage.js
@@ -18,7 +18,7 @@ const LandingPage = () => {
         </div>
         <button>
           <p>Scroll to Bottom</p>
-            <svg stroke-width="4" stroke="currentColor" viewBox="0 0 24 24" fill="none" class="h-6 w-6" xmlns="http://www.w3.org/2000/svg">
+          <svg stroke-width="4" stroke="currentColor" viewBox="0 0 24 24" fill="none" class="h-6  w-6" xmlns="http://www.w3.org/2000/svg" transform="rotate(90)">
             <path d="M14 5l7 7m0 0l-7 7m7-7H3" stroke-linejoin="round" stroke-linecap="round"></path></svg>
         </button>
       </div>
