b8b4d688ce92b3320f1886df22fc728097a82c3a
Shobhan-Pakhira <87083191+Shobhan-Pakhira@users.noreply.github.com>
2023-01-25 13:19:45 +0530

    *

--- scrollreact/src/App.js
+++ scrollreact/src/App.js
@@ -2,25 +2,18 @@ import './App.css';
 import React from 'react';
 import Hero from './components/Hero';
 import SecondPage from './components/Secondpage';
-import ProductDisplay from './components/ProductDisplay';
-import ShowHide from './components/ShowHide';
 import LandingPage from './components/LandingPage';
 import CardPage from './components/CardPage';
 import FeaturePage from './components/FeaturePage';
-import CardClick from './components/CardClick';
-
 
 function App() {
   return (
     <>
       <Hero/>
       <SecondPage/>
-      {/* <ProductDisplay/> */}
-      {/* <ShowHide /> */}
       <LandingPage />
       <CardPage />
       <FeaturePage />
-      <CardClick />
     </>
   );
 }
