f44fe3d7be0f0d26f448e05da2a0bdecd30806e2
Shobhan-Pakhira <87083191+Shobhan-Pakhira@users.noreply.github.com>
2023-01-27 16:54:51 +0530

    .

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import FeatureDisplay from './components/FeatureDisplay/FeatureDisplay';
 import LandingPage from './components/LandingPage/LandingPage';
 import CardPage from './components/CardPage/CardPage';
 import FeaturePage from './components/FeaturePage/FeaturePage';
+import SupportFeature from './components/SupportFeature/SupportFeature'
+import HomePage from './components/HomePage/HomePage';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
       <LandingPage />
       <CardPage />
       <FeaturePage />
+      {/* <SupportFeature/>
+      <HomePage/>
+      <Footer/> */}
     </>
   );
 }
