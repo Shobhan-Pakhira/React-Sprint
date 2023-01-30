29417901f38e59efed5ad048904f0086d4488454
Shobhan-Pakhira <87083191+Shobhan-Pakhira@users.noreply.github.com>
2023-01-30 20:28:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,22 +4,23 @@ import React from 'react';
 // import LandingPage from './components/LandingPage/LandingPage';
 // import CardPage from './components/CardPage/CardPage';
 // import FeaturePage from './components/FeaturePage/FeaturePage';
-import SupportFeature from './components/SupportFeature/SupportFeature';
-import Footer from './components/Footer/Footer';
-import HomePage from './components/HomePage/HomePage'
+// import SupportFeature from './components/SupportFeature/SupportFeature';
+// import Footer from './components/Footer/Footer';
+// import HomePage from './components/HomePage/HomePage'
+import ScrollCard from './components/ScrollCard/ScrollCard'
 
 function App() {
   return (
     <>
+      {/* <LandingPage /> */}
      {/* <ScrollAnimation/>
       <FeatureDisplay/>
-      <LandingPage />
-      {/* <ScrollCard/> */}
       {/* <CardPage /> */}
       {/* <FeaturePage /> */}
-      <SupportFeature /> 
-      <Footer />
-      <HomePage /> 
+      {/* <SupportFeature />  */}
+      {/* <Footer /> */}
+      {/* <HomePage />  */}
+      <ScrollCard/>
     </>
   );
 }
