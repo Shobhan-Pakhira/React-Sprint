db8589f4385dd4ad69fecfd90236475cad288f6b
Kanchan <kanchan08642@gmail.com>
2023-01-30 17:50:14 +0530

    footer updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import ScrollAnimation from './components/ScrollAnimation/ScrollAnimation';
-import FeatureDisplay from './components/FeatureDisplay/FeatureDisplay';
-import LandingPage from './components/LandingPage/LandingPage';
-import CardPage from './components/CardPage/CardPage';
-import FeaturePage from './components/FeaturePage/FeaturePage';
-import SupportFeature from './components/SupportFeature/SupportFeature';
+// import ScrollAnimation from './components/ScrollAnimation/ScrollAnimation';
+// import FeatureDisplay from './components/FeatureDisplay/FeatureDisplay';
+// import LandingPage from './components/LandingPage/LandingPage';
+// import CardPage from './components/CardPage/CardPage';
+// import FeaturePage from './components/FeaturePage/FeaturePage';
+// import SupportFeature from './components/SupportFeature/SupportFeature';
 import Footer from './components/Footer/Footer';
-import HomePage from './components/HomePage/HomePage'
+// import HomePage from './components/HomePage/HomePage'
 
 function App() {
   return (
     <>
-      {/* <ScrollAnimation/>
+     {/* <ScrollAnimation/>
       <FeatureDisplay/>
       <LandingPage />
       <CardPage />
-      <FeaturePage /> */}
-      <SupportFeature />
+      <FeaturePage />
+      <SupportFeature /> */}
       <Footer />
       {/* <HomePage /> */}
     </>
