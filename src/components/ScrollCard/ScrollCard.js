c49ad85fd94e892d3a59b2fbc1a6b8255f515f06
Shobhan-Pakhira <87083191+Shobhan-Pakhira@users.noreply.github.com>
2023-01-31 22:01:07 +0530

    Minor Fixes

--- src/components/ScrollCard/ScrollCard.js
+++ src/components/ScrollCard/ScrollCard.js
@@ -1,51 +1,52 @@
 import React from "react";
 import './ScrollCard.css'
-import CardImage from "../../assets/ScrollCard/see.png";
+import see from "../../assets/ScrollCard/see.png";
+import understand from '../../assets/ScrollCard/understand.png'
+import enable from '../../assets/ScrollCard/enable.png'
 
 const cardData = [
 	{
-		image: CardImage,
+		image: see,
 		title: "Gain Content",
 		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
 	},
 	{
-		image: CardImage,
-		title: "Gain Content",
+		image: understand,
+		title: "Address Blind Spots",
 		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
 	},
 	{
-		image: CardImage,
-		title: "Gain Content",
+		image: enable,
+		title: "Eliminate Universal Risk",
 		info: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
 	},
 ];
 
 const ScrollCard = () => {
 	return (
-		<div className="container-fluid">
-			<div className="col-lg-12 col-md-8 col-sm-6 d-flex flex-column justify-content-center align-items-center">
-				<h1 className="text-center pb-5 pt-2 mt-2">
+		<div className="container-fluid p-0">
+			<div className="col-lg-12 col-md-8 col-sm-6 d-flex flex-column justify-content-center align-items-center" style={{backgroundColor:"#0a1628"}}>
+				<h1 className="text-center text-light pb-4 pt-2 mt-2">
 					<strong>
-						Gain Context, Address Blind Spots, <br /> and Eliminate Universal
-						Risk
+						Gain Context, Address Blind Spots, <br /> and Eliminate Universal Risk
 					</strong>
 				</h1>
 				<div className="container">
 					<div className="col-12">
 						{cardData.map((data, idx) => {
 							return (
-								<div className="col-12 bg-dark rounded-4 d-flex flex-row justify-content-between align-items-center my-5 sticky-card">
-									<img src={CardImage} alt="img" style={{ height: "550px" }} />
+								<div className="col-12 rounded-4 d-flex flex-row justify-content-between align-items-center mb-5 sticky-card">
+									<img src={data.image} alt="img" style={{ height: "550px" }} />
 									<div
-										className="col-6 d-flex flex-column text-light"
+										className="d-flex flex-column text-light px-2"
 										style={{ paddingRight: "150px" }}>
 										  <h2
 											  className="display-5 mb-3"
 											  style={{ fontSize: "45px", fontWeight: "500" }}>
-											    Gain Content
+											    {data.title}
 										  </h2>
 										  <p style={{ fontSize: "18px" }}>
-											  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+											  {data.info}
 										  </p>
 									</div>
 								</div>
