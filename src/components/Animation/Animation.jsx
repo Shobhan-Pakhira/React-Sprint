7de2d9b82f611cfc4964f2a2f7111a51e30b5f27
Kanchan <kanchan08642@gmail.com>
2023-01-31 19:59:57 +0530

    commit

--- src/components/Animation/Animation.jsx
+++ src/components/Animation/Animation.jsx
@@ -1,33 +1,41 @@
 import React,{useRef} from 'react'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 import './Animation.css'
-import mountainbg from "../../assets/Animation/mountainbg.jpg"
-import mountain1 from "../../assets/Animation/mountain1.png"
-import mountain2 from "../../assets/Animation/mountain2.png"
+// import mountainbg from "../../assets/Animation/mountainbg.jpg"
+// import mountain1 from "../../assets/Animation/mountain1.png"
+// import mountain2 from "../../assets/Animation/mountain2.png"
+import mountainsbehind from "../../assets/Animation/mountainsbehind.png"
+import mountainsfront from "../../assets/Animation/mountainsfront.png"
+import moon from "../../assets/Animation/moon.png"
+import stars from "../../assets/Animation/stars.png"
+
 
 const Animation = () => {
   const ref = useRef()
   return (
     <Parallax pages={3} ref={ref} className="animation">
       <ParallaxLayer offset={0} speed={1.5}>
-        {/* <div className='mountainbg'></div> */}
-        <img src={mountainbg}/>
+       
+        <img className='bg-dark w-100 bg-secondary bg-gradient' src={mountainsbehind}/>
       </ParallaxLayer>
 
       <ParallaxLayer offset={0} speed={2.0} >
+      <img className='w-75' src={stars}/>
        
-        {/* <div className='mountain1'></div> */}
       </ParallaxLayer>
-
+{/* 
        <ParallaxLayer offset={0} speed={2.0} >
-       <img src={mountain1}/>
-        {/* <div className='mountain1'></div> */}
+       <img className='w-75' src={moon}/>
+        
+      </ParallaxLayer> */}
+
+      <ParallaxLayer offset={0} speed={2.0} >
+       <img className='w-100' src={mountainsfront}/>
+        
       </ParallaxLayer>
 
-      <ParallaxLayer offset={0} speed={2.0}>
-      <img src={mountain2}/>
-        {/* <div className='mountain2'></div> */}
-      </ParallaxLayer> 
+
+ 
 
       {/* <ParallaxLayer offset={1} speed={-2} factor={1.5} horizontal />
 
