ee376a7d5dadcb9338dbc32024e416e8563d61e6
Shobhan-Pakhira <87083191+Shobhan-Pakhira@users.noreply.github.com>
2023-02-01 17:38:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,23 @@
 import React from 'react';
 import ScrollCard from './components/ScrollCard/ScrollCard';
-// import ScrollAnimation from './components/ScrollAnimation/ScrollAnimation';
-// import FeatureDisplay from './components/FeatureDisplay/FeatureDisplay';
+import ScrollAnimation from './components/ScrollAnimation/ScrollAnimation';
+import FeatureDisplay from './components/FeatureDisplay/FeatureDisplay';
 // import LandingPage from './components/LandingPage/LandingPage';
 // import CardPage from './components/CardPage/CardPage';
 // import FeaturePage from './components/FeaturePage/FeaturePage';
-import SupportFeature from './components/SupportFeature/SupportFeature';
-import Footer from './components/Footer/Footer';
-import HomePage from './components/HomePage/HomePage'
-import Animation from './components/Animation/Animation';
+// import SupportFeature from './components/SupportFeature/SupportFeature';
+// import Footer from './components/Footer/Footer';
+// import HomePage from './components/HomePage/HomePage'
+// import Animation from './components/Animation/Animation';
 
 
 function App() {
   return (
     <>
-      {/* <ScrollAnimation/>
-      <FeatureDisplay/>
-      <LandingPage />
-      <CardPage />
-  <FeaturePage />  
-       <Animation /> */}
-      {/* <SupportFeature /> 
-      <Footer />
-      
-
-       */}
-
-{/* <HomePage /> 
-<Footer />
-<SupportFeature /> */}
-<Animation />
-    </>
+      <ScrollAnimation />
+      <ScrollCard />
+      <FeatureDisplay />
+    </>  
   );
 }
 
